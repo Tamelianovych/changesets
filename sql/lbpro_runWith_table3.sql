3fbb8faec4728c4980eefc87e6dc0e8cac134655
Taras Amelianovych <97164517+Tamelianovych@users.noreply.github.com>
2024-05-14 10:31:59 +0200

    Create lbpro_runWith_table3.sql

---
+++ sql/lbpro_runWith_table3.sql
@@ -0,0 +1,10 @@
+	CREATE TABLE managers3
+	  (
+		manager_id VARCHAR( 255 ) NOT NULL,
+		first_name VARCHAR( 255 ) NOT NULL,
+		last_name  VARCHAR( 255 ) NOT NULL,
+		email      VARCHAR( 255 ) NOT NULL,
+		phone      VARCHAR( 50 ) NOT NULL ,
+		hire_date  DATE NOT NULL          ,
+		job_title  VARCHAR( 255 ) NOT NULL
+	  )
